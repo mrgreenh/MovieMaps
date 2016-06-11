daa0c39620a8c256927c1294af0224e26cfc15d9
Carlo Andrea Conte <carloandreaconte@icloud.com>
2016-06-10 19:27:06 -0700

    test

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import make_response
 
-print "--------- HELLOO"
-
 MONGO_URL = os.environ.get('MONGO_URL')
 if not MONGO_URL:
     MONGO_URL = "mongodb://localhost:27017/test";
@@ -20,6 +18,3 @@ mongo = PyMongo(app)
 def index():
     resp = make_response(dumps(mongo.db.test.find_one_or_404({"_id": ObjectId("575a6aac94c78c5d1895db9a")}).get("value")), 200)
     return resp
-
-
-app.run(debug=True)
